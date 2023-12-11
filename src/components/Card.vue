302cffe60dba917db6e1dc6fafd56553756460ce
alicedev94 <alicedev94@gmail.com>
2023-12-10 23:41:03 -0400

    paso a vue3 con vuetify

---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card
+    width="400"
+    title="Portafolio"
+    subtitle="Esto es un portafolio"
+    text="Portafolio con vue3 y vuetify"
+  ></v-card>
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    children: [
+      {
+        path: '',
+        name: 'Card',
+        // route level code-splitting
+        // this generates a separate chunk (Home-[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/components/Card.vue'),
+      },
+    ],
+  },
+  {
+    path: '/home',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        // route level code-splitting
+        // this generates a separate chunk (Home-[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/Home.vue'),
+      },
+    ],
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
